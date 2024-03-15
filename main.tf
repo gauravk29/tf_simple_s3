b7655fde85e362ece2e378c6b399228c6909fe44
gauravk29 <73769319+gauravk29@users.noreply.github.com>
2024-03-15 13:54:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "spacelift-test-bucket"
+    Name        = "spacelift-test-bucket-slalom-001"
     Environment = "Dev"
   }
 }
